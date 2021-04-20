8f71eaa0de4995bac0c8b4fa910d37e916f4408f
Aananthi-CA0236 <aananthi.m@credavenue.com>
2021-04-20 15:24:07 +0530

    follwers

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,30 @@ class User < ApplicationRecord
 	validates :email, presence: true, length: { maximum: 255},format: { with: VALID_EMAIL_REGEX },uniqueness: true
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 } , allow_nil: true
-
+	has_many :active_relationships, class_name: "Relationship",foreign_key: "follower_id",dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+	has_many :followers, through: :passive_relationships, source: :follower
+	
 	# Defines a proto-feed.
 # See "Following users" for the full implementation.
 def feed
 Micropost.where("user_id = ?", id)
 end
-
+# Follows a user.
+def follow(other_user)
+	following << other_user
+end
+# Unfollows a user.
+def unfollow(other_user)
+	following.delete(other_user)
+end
+# Returns true if the current user is following the other user.
+def following?(other_user)
+	puts other_user.name
+	puts following
+	following.include?(other_user)
+end
 	# Returns the hash digest of the given string.
 	def User.digest(string)
 		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
